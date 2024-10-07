d4b9ceb1934b0a4cc7ef3526a2706086b18b8610
selam <solhadush.27@gmail.com>
2024-10-07 22:33:58 +0300

    Banner

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,13 +1,13 @@
-const API_KEY = proscess.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-  fetchTrending: "/trending/all/day?api_key=${API_KEY}&language=en-US",
-  fetchNetflixOriginals: "/discover/tv?api_key=${API_KEY}&with_networks=213",
-  fetchTopRatedMovies: "top_rated?api_key=${API_KEY}&language=en-US&page=1",
-  fetchActionMovies: "/discover/movie?api_key=${API_KEY}&with_genres=28",
-  fetchComodyMovies: "/discover/movie?api_key=${API_KEY}&with_genres=35",
-  fetchHorrorMovies: "/discover/movie?api_key=${API_KEY}&with_genres=27",
-  fetchRomanceMovies: "/discover/movie?api_key=${API_KEY}&with_genres=10749",
-  fetchDocumentaries: "/discover/movie?api_key=${API_KEY}&with_genres=99",
-  fechTvShow: "tv/popular?api_key=${API_KEY}&language=en-US&page=1",
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComodyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, // Fixed typo from "Comody"
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fechTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`, // Fixed typo from "fechTvShow"
 };
 export default requests;
